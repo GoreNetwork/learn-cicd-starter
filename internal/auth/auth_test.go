9f13c764280b048f18ca836635daf8ca93ae02ad
Daniel Himes <dhimes@gmail.com>
2025-06-10 13:52:32 -0400

    test

--- internal/auth/auth_test.go
+++ internal/auth/auth_test.go
@@ -2,12 +2,12 @@ package auth
 
 import "testing"
 
-// func TestAlwaysPasses(t *testing.T) {
-//     if true != true {
-//         t.Error("Somehow true isn't true... that's weird!")
-//     }
-// }
-
-func TestAlwaysFails(t *testing.T) {
-    t.Fatal("This test is guaranteed to fail.")
+func TestAlwaysPasses(t *testing.T) {
+    if true != true {
+        t.Error("Somehow true isn't true... that's weird!")
+    }
 }
+
+// func TestAlwaysFails(t *testing.T) {
+//     t.Fatal("This test is guaranteed to fail.")
+// }
